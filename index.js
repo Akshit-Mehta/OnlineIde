9366cfbfa0f6e2ba7c8b94ba5e6bc5273630958f
Akshit-Mehta <2017ucp1432@mnit.ac.in>
2021-07-25 14:31:40 +0530

    Some changes for deployment

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.options('*', cors());
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
 })
 
 app.post('/compiler/c', (req, res) => {
